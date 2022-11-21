b7cb42204f2778859743b2e5545b0cea3c41576b
bys096 <jamesban01@gmail.com>
2022-11-21 09:09:55 +0900

    first commit Create Vue Project Add Components(TodoHeader, TodoFooter, TodoList, TodoInput)

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <input type="text" v-model="newTodoItem">
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
