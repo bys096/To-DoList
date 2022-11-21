2a86e8e623e98a50f8628e4b7ecdb382e9087e5e
bys096 <jamesban01@gmail.com>
2022-11-22 03:53:08 +0900

    Modify TodoInput.vue and TodoList.vue TodoInput.vue	:	Add addTodo method and input style TodoList.vue	:	Add created Life Cycle

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,6 +1,13 @@
 
 <template>
-  <input type="text" v-model="newTodoItem">
+<div class="inputBox shadow">
+  <!-- Enter가 눌리면 addTodo 메서드 실행 -->
+  <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+  <!-- <button @click="addTodo">add</button> -->
+  <span class="addContainer" @click="addTodo">
+    <i class="fa-solid fa-plus addBtn"></i>
+  </span>
+</div>
 </template>
 
 <script>
@@ -9,10 +16,45 @@ export default {
     return {
       newTodoItem: ""
     }
-  }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      // localStorage.setItem()을 통해 저장 후, 초기화
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8773FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>list</div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem">
+        {{ todoItem }}
+      </li>
+
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        // localStorage에 loglevel-webpack이 담겨있으므로 제외
+        if(localStorage.key(i) !== "loglevel:webpack-dev-server"){
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
 }
 </script>
 
